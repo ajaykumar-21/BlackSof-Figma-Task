4d6a5f2c45377c2e45388c324234be5a1c500de4
ajaykumar-21 <ajaykumarpk789@gmail.com>
2024-11-29 21:57:04 +0530

    make navbar responsive for all device

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,57 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
 import SmartLight from "../../public/assets/smart lights_logo.svg";
 import Hero from "./Hero";
 
 const Header: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <>
       <header className="bg-dark-blue shadow-md">
-        <div className="container mx-auto px-32 py-6 flex justify-between items-center">
-          <div>
+        <div className="container mx-auto px-4 md:px-12 lg:px-32 py-4 flex flex-wrap justify-between items-center">
+          {/* Logo Section */}
+          <div className="w-full md:w-auto flex justify-between items-center">
             <Image src={SmartLight} alt="Smart Light" />
+            {/* Hamburger Menu */}
+            <button
+              className="block md:hidden text-white focus:outline-none"
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            </button>
           </div>
-          <nav className="space-x-8">
-            <a href="#" className="text-white-600 hover:text-gray-800">
+
+          {/* Navigation Section */}
+          <nav
+            className={`${
+              isMenuOpen ? "block" : "hidden"
+            } md:flex space-y-4 md:space-y-0 md:space-x-4 lg:space-x-8 w-full md:w-auto mt-4 md:mt-0`}
+          >
+            <a href="#" className="text-white hover:text-gray-400 block">
               Home
             </a>
-            <a href="#" className="text-white-600 hover:text-gray-800">
+            <a href="#" className="text-white hover:text-gray-400 block">
               Products
             </a>
-            <a href="#" className="text-white-600 hover:text-gray-800">
+            <a href="#" className="text-white hover:text-gray-400 block">
               Software Services
             </a>
-            <button className="bg-blue-700 w-28 h-9 rounded hover:bg-gray-800">
+            <button className="bg-blue-700 w-28 h-9 rounded hover:bg-gray-800 text-white">
               Login
             </button>
           </nav>
